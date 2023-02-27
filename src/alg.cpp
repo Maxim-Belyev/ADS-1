9302f3eda44b49eed5da4fba591404502ba664a8
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-02-27 22:42:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,57 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
-  if (value<2) {
-		return false;
-	  }
-	for (int i = 2; i < value; i++) {
-		if (value % i == 0) {
-			return false;
-		}
+    if (value<2) {
+        return false;
+    }
+    for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+            return false;
 	}
-	return true;
+    }
+    return true;
 }
 uint64_t nPrime(uint64_t n) {
-  int Num = 2;
-	int i = 0;
-	while (1) {
-		if (checkPrime(Num) == 1) {
-			i++;
-		}
-		if (i == n) {
-			break;
-		}
-		Num++;
+    uint64_t Num = 2;
+    uint64_t i = 0;
+    while (1) {
+        if (checkPrime(Num) == 1) {
+            i++;
+        }
+        if (i == n) {
+            break;
+        }
+        Num++;
     }
-	return Num;
+    return Num;
 }
 uint64_t nextPrime(uint64_t value) {
-  while (1) {
-	  int k = 1;
-	  value++;
-	  for (int i = 2; i <=value ; i++) {
-		  if (value % i == 0) {
-			  k++;
-      }
+    while (1) {
+        uint64_t k = 1;
+        value++;
+        for (uint64_t i = 2; i <=value ; i++) {
+            if (value % i == 0) {
+                k++;
+	    }
+	}
+        if (k == 2){
+            return value;
+	}
     }
-	  if (k == 2){
-		  return value;
-    }
-  }
 }
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
-	int k = 0;
-	for (int i = 2; i < hbound; i++) {
-		for (int j = 2; j <= i / 2; j++) {
-			if (i % j == 0) {
-				k=k+1;
-			}
-		}
-		if (k == 0) {
-			sum = sum + i;
-		}
-		k = 0;
-	}
-	return sum;
+    uint64_t sum = 0;
+    uint64_t k = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        for (uint64_t j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                k=k+1;
+            }
+        }
+        if (k == 0) {
+            sum = sum + i;
+        }
+        k = 0;
+    }
+    return sum;
 }
